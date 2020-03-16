c3e8757ba00ced463d2f0e1847e2089c099af2f7
fengrongtao <1451104095@qq.com>
2020-03-16 14:54:47 +0800

    sql累积

---
+++ oracle相关/livebos删除流程逻辑.sql
@@ -0,0 +1,78 @@
+--1.²éÑ¯£º
+select * from os_historystep_prev pr where exists(select 1 from os_historystep p where exists(select 1 from select 1 from lc_gdsy_ydgzpj j where j.instid=p.entry_id) and p.id=pr.id)
+select * from os_historystep t where exists(select 1 from select 1 from lc_gdsy_ydgzpj j where j.instid=t.entry_id and j.lcly=0)
+select * from os_currentstep_prev pr where exists(select 1 from os_currentstep p where exists(select 1 from select 1 from lc_gdsy_ydgzpj j where j.instid=p.entry_id and j.lcly=0) and p.id=pr.id)
+select * from os_currentstep p where exists(select 1 from select 1 from lc_gdsy_ydgzpj j where j.instid=p.entry_id and j.lcly=0)
+select * from os_wfentry y where exists(select 1 from lc_gdsy_ydgzpj j where j.instid=y.id and j.lcly=0)
+select * from lc_gdsy_ydgzpj_zxxx xx where exists(select 1 from lc_gdsy_ydgzpj pj where pj.lcly=0 and pj.id=xx.lc_gdsy_ydgzpj_id)
+select * from lc_gdsy_ydgzpj x where x.lcly=0
+--2.É¾³ý£º
+-- delete  
+ --1.
+delete
+  from os_historystep_prev pr
+ where exists (select 1
+          from os_historystep p
+         where exists (select 1
+                 -- from select 1
+                         from lc_gdsy_ydgzpj j
+                        where j.instid = p.entry_id and j.lcly = 0 
+                )
+           and p.id = pr.id); 
+ commit;          
+--2.
+delete
+  from os_currentstep_prev pr
+ where exists (select 1
+          from os_currentstep p
+         where exists (select 1
+                  --from select 1
+                         from lc_gdsy_ydgzpj j
+                        where j.instid = p.entry_id
+                          and j.lcly = 0
+                )
+           and p.id = pr.id);
+    commit;       
+           
+--3.
+delete
+  from os_historystep t
+ where exists (select 1
+          --from select 1
+                 from lc_gdsy_ydgzpj j
+                where j.instid = t.entry_id
+                  and j.lcly = 0
+        );
+  commit;       
+
+           
+--4.
+delete
+  from os_currentstep p
+ where exists (select 1
+          -- from select 1
+                 from lc_gdsy_ydgzpj j
+                where j.instid = p.entry_id
+                  and j.lcly = 0
+        );
+ commit;       
+--5.
+delete
+  from os_wfentry y
+ where exists (select 1
+          from lc_gdsy_ydgzpj j
+         where j.instid = y.id
+           and j.lcly = 0);
+ commit;          
+--6.
+delete
+  from lc_gdsy_ydgzpj_zxxx xx
+ where exists (select 1
+          from lc_gdsy_ydgzpj pj
+         where pj.lcly = 0
+           and pj.id = xx.lc_gdsy_ydgzpj_id);
+commit;          
+--7.
+delete from lc_gdsy_ydgzpj x where x.lcly=0;
+ commit; 
+
