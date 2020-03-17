a6c0c4364a63a02e052d3470cf8b31acbd521ee5
fengrongtao <1451104095@qq.com>
2020-03-17 16:38:17 +0800

    sql累积

--- oracle相关/livebos删除流程逻辑.sql
+++ oracle相关/livebos删除流程逻辑.sql
@@ -39,20 +39,17 @@ select *
                                   from os_wfentry y
                                  where exists (select 1
                                           from lc_gdsy_ydgzpj j
-                                         where j.instid = y.id
-                                           --and j.lcly = 0
-                                           )
+                                         where j.instid = y.id                                         
+                                           );
                                   select *
                                           from lc_gdsy_ydgzpj_zxxx xx
                                          where exists
                                          (select 1
                                                   from lc_gdsy_ydgzpj pj
-                                                 where 
-                                                 --pj.lcly = 0
-                                                  -- and 
+                                                 where                                              
                                                    pj.id =
-                                                       xx.lc_gdsy_ydgzpj_id)
-select * from lc_gdsy_ydgzpj x where x.lcly=0
+                                                       xx.lc_gdsy_ydgzpj_id);
+select * from lc_gdsy_ydgzpj x where x.lcly=0;
 --2.É¾³ý£º
 -- delete  
  --1.
@@ -60,71 +57,59 @@ delete
   from os_historystep_prev pr
  where exists (select 1
           from os_historystep p
-         where exists (select 1
-                 -- from select 1
+         where exists (select 1             
                          from lc_gdsy_ydgzpj j
-                        where j.instid = p.entry_id  -- and j.lcly = 0 
+                        where j.instid = p.entry_id  
                 )
            and p.id = pr.id); 
- --commit;          
+ commit;          
 --2.
 delete
   from os_currentstep_prev pr
  where exists (select 1
           from os_currentstep p
-         where exists (select 1
-                  --from select 1
+         where exists (select 1                
                          from lc_gdsy_ydgzpj j
-                        where j.instid = p.entry_id
-                          --and j.lcly = 0
+                        where j.instid = p.entry_id                         
                 )
-           and p.id = pr.id);
-   -- commit;       
-           
+           and p.id = pr.id);            
 --3.
 delete
   from os_historystep t
  where exists (select 1
-          --from select 1
                  from lc_gdsy_ydgzpj j
-                where j.instid = t.entry_id
-                  --and j.lcly = 0
+                where j.instid = t.entry_id               
         );
- -- commit;       
+  commit;       
 
            
 --4.
 delete
   from os_currentstep p
  where exists (select 1
-          -- from select 1
                  from lc_gdsy_ydgzpj j
                 where j.instid = p.entry_id
-                  --and j.lcly = 0
         );
- --commit;       
+ commit;       
 --5.
 delete
   from os_wfentry y
  where exists (select 1
           from lc_gdsy_ydgzpj j
-         where j.instid = y.id
-           --and j.lcly = 0
+         where j.instid = y.id           
            );
- --commit;          
+ commit;          
 --6.
 delete
   from lc_gdsy_ydgzpj_zxxx xx
  where exists (select 1
           from lc_gdsy_ydgzpj pj
          where 
-            pj.id = xx.lc_gdsy_ydgzpj_id
-            --and pj.lcly = 0
-            
+            pj.id = xx.lc_gdsy_ydgzpj_id            
             );
---commit;          
+commit;          
 --7.
-delete from lc_gdsy_ydgzpj x  -- where x.lcly=0
+delete from lc_gdsy_ydgzpj x  
 ;
  commit; 
 
