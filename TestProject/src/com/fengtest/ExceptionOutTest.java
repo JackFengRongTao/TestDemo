367494fbaffdd34b8373c95aeb47ac15d28c14f4
fengrongtao <1451104095@qq.com>
2020-05-06 15:23:08 +0800

    修改

--- TestProject/src/com/fengtest/ExceptionOutTest.java
+++ TestProject/src/com/fengtest/ExceptionOutTest.java
@@ -1,4 +1,17 @@
 package com.fengtest;
 
 public class ExceptionOutTest {
+    public static void main(String[] args) {
+        try {
+            String a = null;
+            System.out.println(Integer.parseInt(a.trim()));
+        } catch (Exception e) {
+            //输出异常
+            System.out.println(e);
+            //输出异常
+            System.out.println(e.getMessage());
+            //输出异常
+            e.printStackTrace();
+        }
+    }
 }
--- TestProject/src/com/fengtest/ThrowNewExceptionDemo.java
+++ TestProject/src/com/fengtest/ThrowNewExceptionDemo.java
@@ -3,78 +3,44 @@ package com.fengtest;
 /**
  * 抛出新异常并被捕获
  */
-public class ThrowNewExceptionTest {
-
-
+public class ThrowNewExceptionDemo{
+    /**
+     *
+     * 主线程可以先结束，异步线程去处理通知相关人员来处理-
+     */
     public static void main(String[] args) {
         try {
-            test0();
+            exceptionFunction();
         } catch (Exception e) {
             if(e.getMessage().contains("[testError]")){
-                System.out.println("起线程");
-                AsyncNotifyThread asyncNotifyThread = new AsyncNotifyThread();
+                System.out.println("起异步线程通知相关人员去处理.");
+                AsyncNotifyThreadDemo asyncNotifyThread = new AsyncNotifyThreadDemo();
                 Thread thread = new Thread(asyncNotifyThread);
                 thread.start();
             }
         }
-        System.out.println("结束");
-    }
-    public static void test0() throws Exception {
-        try {
-            int i1 = test();
-            int i3 = test3();
-        } catch (Exception e) {
-            throw new Exception("[testError]"+e.getMessage());
-        }
-    }
-    public static int test() throws Exception {
-        try {
-            int i = 1/0;
-        } catch (Exception e) {
-            throw  new Exception(e.getMessage());
-        }finally {
-            test2();//重新初始化
-        }
-        System.out.println("1");
-        return 1;
-    }
-    public static int test2() {
-
-        try {
-            int i = Integer.parseInt("hhh");
-        } catch (Exception e) {
-             e.printStackTrace();
-        }
-        System.out.println("重新初始化...");
-        return 2;
+        System.out.println("main结束执行.");
     }
 
-    public static int test3() throws Exception {
-        System.out.println("进来test3，执行接口调用");
+    public static void exceptionFunction() throws Exception {
         try {
-            int i = Integer.parseInt("3hhh");
+            int i = 1/0;//模拟调用接口出异常
         } catch (Exception e) {
-            throw  new Exception("test3Error");
+            throw  new Exception("[testError]"+e);
         }
-        System.out.println("执行接口调用");
-        return 3;
+        System.out.println("exceptionFunction结束执行.");
     }
-
 }
 
-class AsyncNotifyThread implements Runnable{
-        @Override
-        public void run() {
-            System.out.println("请您看下接口有无问题...");
-            int count = 4;//从redis取值
-            while (count > 0){
-                try {
-                    Thread.sleep(60*1000);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-                System.out.println("请您看下接口有无问题...");
-                count--;
-            }
+class AsyncNotifyThreadDemo implements Runnable{
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3*1000);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        //模拟发短信或者邮件
+        System.out.println("xxx，您好，请您看下接口有无问题...");
+    }
 }
